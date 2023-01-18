22ae3167fcedaf62fa47da601aaabeede2a52afb
ocastrup <ole.christian.astrup@dnv.com>
2023-01-18 11:33:23 +0100

    test: Added tests for main and schema CLI.

---
+++ tests/test_schema_cli.py
@@ -0,0 +1,15 @@
+#  Copyright (c) 2023. OCX Consortium https://3docx.org. See the LICENSE
+#
+from click.testing import CliRunner
+from ocx_tools.cli.cli_main import cli, schema
+
+
+def test_schema():
+    runner = CliRunner()
+    result = runner.invoke(cli, ['schema'])
+    assert result.exit_code == 0
+
+def test_schema_parse():
+    runner = CliRunner()
+    result = runner.invoke(cli,['schema','parse'])
+    assert result.exit_code == 0
